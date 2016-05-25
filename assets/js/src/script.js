c1f30b0c486854c417ddd5a6f1ddfdd81b531c91
Steven Mann <stevenmann@UKLEMC65.local>
2016-05-25 11:08:54 +0100

    [initial] commit

---
+++ assets/js/src/script.js
@@ -0,0 +1,44 @@
+
+$(function(){
+
+	// API call
+	$.get('http://api.fixer.io/latest?base=ZAR', function(data){
+
+		var countryObject = data.rates;
+
+		// Loop through object, get keys and values
+		for(var key in countryObject){
+			if(countryObject.hasOwnProperty(key)) {
+				$('select').append('<option val="' + key + '">' + key + '</option>');
+			}
+		}
+
+		// Second API call upon changing base currency
+		$('.convertFrom').change(function(){
+			var thisValue = $('.convertFrom').val();
+
+			$.get('http://api.fixer.io/latest?base=' + thisValue, function(newData){
+				countryObject = newData.rates;
+			});
+		});
+
+		// Conversion calculation
+		$('form').on('submit', function(e){
+			e.preventDefault();
+
+			var convertingTo = $('.convertTo').val(),
+				toRate = countryObject[convertingTo],
+				amount = $('.currencyValue').val();
+
+			var calculation = amount * toRate;
+
+			if(calculation === 0) {
+				$('.output').html('Please enter a valid amount.');
+			} else {
+				$('.output').html(calculation);
+			}
+		});
+
+	});
+
+});
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<title>Currency Converter</title>
+
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css" />
+	</head>
+
+	<body>
+
+		<header>
+			<h1>Currency Converter</h1>
+		</header>
+
+
+		<form>
+
+			<select class="convertFrom">
+			</select>
+
+
+			<input type="number" class="currencyValue">
+
+			<select class="convertTo">
+			</select>
+
+			<input type="submit" value="convert">
+		</form>
+
+		<p class="output"></p>
+
+	</body>
+
+</html>
+
+
+
+<script   src="https://code.jquery.com/jquery-2.2.4.min.js"   integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="   crossorigin="anonymous"></script>
+<script type="text/javascript" src="assets/js/src/script.js"></script>
